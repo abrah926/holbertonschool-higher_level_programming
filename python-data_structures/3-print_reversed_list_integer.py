c1a5d5f4b6518009bceea4af8a9246e169dd904d
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-09 16:08:44 -0400

    sintax errors corrected

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-        my_listlist.reverse(my_list)
+        my_list.reverse(my_list)
 
     for i in my_list:
         print("{:d}".format(i))
