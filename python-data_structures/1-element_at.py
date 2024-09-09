5736c22f49bde6c360b19b05a35c7e0b216b9219
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-09 10:51:27 -0400

    script that retrieves elems of a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,9 +3,6 @@
 
 def element_at(my_list, idx):
     for idx in my_list:
-        if idx <= 0:
-            break
+        if idx < 0 or idx > len(my_list):
             return None
-        if idx == len(my_list) + 1:
-            return None
-    print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    print(my_list[idx])
