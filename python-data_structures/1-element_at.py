3bab094f6f31a4cb6d4f8d4c75a78daeba35379d
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-09 10:46:11 -0400

    script that retrieves elems of a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,6 +4,7 @@
 def element_at(my_list, idx):
     for idx in my_list:
         if idx <= 0:
+            break
             return None
         if idx == len(my_list) + 1:
             return None
