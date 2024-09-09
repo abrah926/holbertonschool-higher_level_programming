8d43e91555cbc357c98769272716a2669d3b7ebc
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-09 16:04:50 -0400

    new line addded at eof

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,9 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    list.reverse(my_list)
+    if my_list:
+        my_listlist.reverse(my_list)
 
     for i in my_list:
         print("{:d}".format(i))
+        
