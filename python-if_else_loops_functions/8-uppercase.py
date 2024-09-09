df04eea89d181e0e2ce846b42c24b083d561e291
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-09 00:04:22 -0400

    script that prints if lower to upper case

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 
 def uppercase(str):
     for char in str:
-        if 'a' <= char <= 'z'
+        if 'a' <= char <= 'z':
             char = chr(ord(char) -32)
          print("{}".format(char), end="")
     print()
