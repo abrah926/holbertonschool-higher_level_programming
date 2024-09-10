9ae8dc5e53b5fec0f5ad8228a8af739fb9d583aa
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-10 13:50:19 -0400

    returns lenght of tuple and its first char

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    len_new_tuple = len(sentence)
+    if len_new_sentence == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+
+    return (len_new_tuple, first_char)
