b7f12b8fbdfd4bdd9f145bf875fac701cce11b76
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-10 13:52:45 -0400

    fixing sintax

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 
 def multiple_returns(sentence):
     len_new_tuple = len(sentence)
-    if len_new_sentence == 0:
+    if len_new_tuple == 0:
         first_char = None
     else:
         first_char = sentence[0]
