156f08ae1d4b9a01c3cfd992bef43d3d408bceb5
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-10 15:21:00 -0400

    debugging

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,10 +2,11 @@
 
 
 def delete_at(my_list=[], idx=0):
-
+    # Check if the index is out of range
     if idx < 0 or idx >= len(my_list):
         return my_list
-    
+
+    # Create a new list excluding the item at the specified index
     new_list = [item for i, item in enumerate(my_list) if i != idx]
-    
+
     return new_list
