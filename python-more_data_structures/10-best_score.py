736acdcd2b3dcb6d9c3c140c212a6d720a52db46
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-12 14:22:14 -0400

    finds max valued key in a dict

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,7 +7,7 @@ def best_score(a_dictionary):
         return None
     
     best_key = None
-    max_value = 0
+    max_value = float('-inf')
 
     for key, value in a_dictionary.items():
         if value > max_value:
