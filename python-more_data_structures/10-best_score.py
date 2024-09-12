39d149e0cfe42fcb628fc6f92ae092bfce356b99
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-12 14:17:46 -0400

    returns biggest valued key in a dict

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+
+    if not a_dictionary:
+        return None
+    
+    best_key = None
+    max_value = 0
+
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            best_key = key
+            max_value = value
+
+        return best_key
