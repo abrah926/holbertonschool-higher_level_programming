ad44c55a802727d90c986fa3248e3e19a8bc80d8
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-18 13:35:56 -0400

    testing if var is int if not returns value error and casts fl into ints

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module to add two integers.
+
+The function `add_integer` takes two numbers and returns their sum after casting
+them to integers if necessary. It raises a TypeError if the inputs are not valid.
+"""
+
+def add_integer(a, b=98):
+    """Add two integers or floats.
+
+    Args:
+        a: First number.
+        b: Second number (default is 98).
+
+    Returns:
+        The sum of `a` and `b` as an integer.
+
+    Raises:
+        TypeError: If `a` or `b` are not integers or floats.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    return int(a) + int(b)
