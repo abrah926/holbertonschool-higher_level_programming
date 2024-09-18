291c87249bef7bb37ead4f30dc412948ad83bef5
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-18 11:40:49 -0400

    cks for type and value errors

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,17 +4,17 @@
 class Square:
     '''Class that defines a square and the size of its sides'''
     
-    def __init__(self, size=0)
-    '''
-    Initializes the square size to zero
+    def __init__(self, size=0):
+        '''
+        Initializes the square size to zero
 
-    Args:
-        size (int): The size of the square.
+        Args:
+            size (int): The size of the square.
 
-    Raises:
-        TypeError: If size isnt an integer.
-        ValueError: If size is less than zero. 
-    '''
+        Raises:
+            TypeError: If size isnt an integer.
+            ValueError: If size is less than zero. 
+        '''
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
