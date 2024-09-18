1dac1ff34a00f12c680d37ac1060b879d7fa3004
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-17 20:36:59 -0400

    initializing class with a private attr

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+'''This module defines a class Square with a private instance attribute'''
 
+class Square:
+    '''
+    A class that defines a square by its size
 
+    Attributes:
+        __size (int): The size of the square (private).
+    '''
+
+    def __init__(self, size):
+        '''
+        Initializes the square with a private size attr.
+        '''
+        self.__size = size
