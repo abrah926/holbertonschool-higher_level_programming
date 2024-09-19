baca390440339cb72c03162c14e3727fa6655a15
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-18 20:38:56 -0400

    class that defines a sqr and returns its size ** 2

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-class Square:
-    """A class that defines a square."""
-    
-    def __init__(self, size=0):
-        """Initialize a new Square.
+"""Define a Square class."""
 
-        Args:
-            size (int): The size of the square (must be an integer).
-        
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
+
+class Square:
+    """Class Square with a size attribute."""
+    def __init__(self, size=0):
         """
+            Initialize a new instance of the Square class.
+            size (int): The size of the square.
+        """
+        self.__size = size
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
 
     def area(self):
-        """Calculate and return the area of the square.
-
-        Returns:
-            int: The area of the square.
         """
-        return self.__size * self.__size
+            Initialize a new instance of the square class.
+            the current square area.
+        """
+        return self.__size ** 2
