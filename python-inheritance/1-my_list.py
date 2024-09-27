bc19045bc75236a17e678c3ad5cadf4cdd2c45c8
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-26 22:44:25 -0400

    prints sorted subclass list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-
-
-"""Module defining the MyList class, inheriting from list."""
+'''
+A class MyList that inherits from list
+'''
 
 
 class MyList(list):
-    """A subclass of list with an additional method to print a sorted list."""
+    '''
+    MyList class
+    '''
 
     def print_sorted(self):
-        """Prints the list in ascending order without modifying the original list."""
         print(sorted(self))
