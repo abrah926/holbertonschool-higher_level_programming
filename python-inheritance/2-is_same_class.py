224342282569958e3127d3caec327d0d5ab9238d
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-09-26 22:54:54 -0400

    debugging

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-Write a function that returns True if the object is exactly
+it returns True if the object is exactly
 an instance of the specified class; otherwise False.
 '''
 
