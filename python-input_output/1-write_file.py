f38245256daa8a3f3ba3cead57e698297e2a026b
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-01 12:10:29 -0400

    providing pep8 standard

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,7 +2,8 @@
 
 
 """
-This module contains a function `write_file` that writes a string to a text file.
+This module contains a function `write_file`
+that writes a string to a text file.
 """
 
 
@@ -12,7 +13,8 @@ def write_file(filename, text=""):
 
     Args:
         filename (str): The name of the file to write to.
-        text (str, optional): The string to write to the file. Defaults to "".
+        text (str, optional): The string to write to the file.
+        Defaults to "".
     """
     with open(filename, "w", encoding='utf-8') as file:
         return file.write(text)
