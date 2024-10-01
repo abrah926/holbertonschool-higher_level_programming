e907c9ee5b02dfddb0dea9697bcd667a44caf8a1
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-01 14:01:32 -0400

    debugging

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,11 @@ and saves them to a JSON file named add_item.json.
 """
 
 import sys
-from 6-load_from_json_file import load_from_json_file
-from 5-save_to_json_file import save_to_json_file
+import importlib
+load_from_json_file = importlib.import_module(
+    '6-load_from_json_file').load_from_json_file
+save_to_json_file = importlib.import_module(
+    '5-save_to_json_file').save_to_json_file
 
 filename = 'add_item.json'
 
