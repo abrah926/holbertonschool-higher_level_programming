41a6d5ae806080324d4feb3f2237ec2c868aa349
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-02 07:32:38 -0400

    serializes student class

---
+++ python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+class Student:
+    """
+    Defines a student with attributes first_name, last_name, and age.
+
+    Methods:
+        to_json: Retrieves the dictionary representation of the Student instance.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+          """
+        Retrieves the dictionary representation of the Student instance.
+
+        Returns:
+            dict: The dictionary containing all the instance's attributes.
+        """
+        return self.__dict__
