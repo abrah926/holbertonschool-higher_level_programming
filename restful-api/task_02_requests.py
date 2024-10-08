1ae66444eedc22f7741b6c2c887e7dbe8b157e9b
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-08 15:15:19 -0400

    updating file

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
 
-import request
+import requests
 import json
 import csv
 
 
 def fetch_and_print_posts():
-    response = response.getresponse(https: // jsonplaceholder.typicode.com/posts)
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+    print(f"Status code: " {response.status_code})
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(f"Title: {post.title")
