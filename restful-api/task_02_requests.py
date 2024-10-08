429f9dd7eadf77996fb58a063fbaef896cdc5bdb
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-08 16:57:37 -0400

    function requests.get, returns status also function to fetch all posts from jsonplaceholder, writes the data into csv file with dict keys

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -14,4 +14,22 @@ def fetch_and_print_posts():
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
-            print(f"Title: {post.title")
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+    if response.status_code == 200:
+        post = response.json()
+
+        structured_data = [
+            {'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+        with open('post.csv', 'w', newline='') as csv_file:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(structured_data)
+
+    print("Post saved to post.csv")
