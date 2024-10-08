e7d5b15ad609780df127d17c9b9febad3cc2ead1
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-08 17:12:00 -0400

    debugging

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python
-
-
 import requests
-import json
 import csv
 
 
 def fetch_and_print_posts():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-
-    print(f"Status code: " {response.status_code})
-
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
@@ -19,17 +14,14 @@ def fetch_and_print_posts():
 
 def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
-
+    response = requests.get(url)
     if response.status_code == 200:
-        post = response.json()
-
+        posts = response.json()
         structured_data = [
             {'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
-        with open('post.csv', 'w', newline='') as csv_file:
+        with open('posts.csv', mode='w', newline='') as csv_file:
             fieldnames = ['id', 'title', 'body']
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
             writer.writeheader()
             writer.writerows(structured_data)
-
-    print("Post saved to post.csv")
+        print("Posts saved to posts.csv")
