e19bb3870578850b2d3cc4c50c544d245199df36
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-08 14:37:30 -0400

    “mensaje”

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+import request
+import json
+import csv
+
+
+def fetch_and_print_posts():
+    response = response.getresponse(https: // jsonplaceholder.typicode.com/posts)
