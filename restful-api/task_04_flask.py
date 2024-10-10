30d1697d09a56173d881cb0d0019003201f0075d
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-10 19:47:40 -0400

    implementing flask methods and posting new users

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -18,5 +18,30 @@ def get_users():
     return jsonify(users)
 
 
+@app.route('/status', methods=['GET'])
+def status():
+    return 'OK'
+
+
+@app.route('/users/<username>', methods=['GET'])
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    username = data.get('username')
+    if username in users:
+        return jsonify({'error': 'User already exists'}), 400
+    users[username] = {'name': data.get('name'), 'age': data.get(
+        'age'), 'city': data.get('city')}
+    return jsonify({'message': 'User added successfully', 'user': users[username]}), 201
+
+
 if __name__ == "__main__":
     app.run(port=5000)
