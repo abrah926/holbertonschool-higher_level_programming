ff7b46abaac3a1d4f725f53460f6673a00972b38
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-10 18:46:52 -0400

    “mensaje”

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
+
+users = {
+
+    'jane': {'name': 'Jane', 'age': 28, 'city': 'Los Angeles'},
+    john: {'name': 'John', 'age': 32, 'city': 'New York'},
+}
 
 
 @app.route('/')
 def home():
     return 'Welcome to Flask API!'
+
+
+@app.route('/data', methods=['GET'])
+def get_users():
+    return jsonify(users)
 
 
 if __name__ == '__main__':
