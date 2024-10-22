bddf0d16d33f27005484c697a17e986c8b3772f5
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-22 10:19:01 -0400

    Creates new table in database

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Content of the file: Creates table in mysql database
-CREATE TABLE IF NOT EXIST first_table (id INT, VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, VARCHAR(256));
