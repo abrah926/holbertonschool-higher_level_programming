da87b0b4ddce535d1b549dac88a91944e5384aa7
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-23 20:53:43 -0400

    script that lists all privileges of the MySQL users user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,4 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost';
-
-CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost';
 
 SHOW GRANTS FOR 'user_0d_1' @'localhost';
 
