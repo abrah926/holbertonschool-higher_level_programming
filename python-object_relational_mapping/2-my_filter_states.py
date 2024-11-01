e0cb8d7f2d254e63c7b5e93f6c86ccd0e6891382
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-11-01 11:31:50 -0400

    Displays all values in the states table where the name matches the input argument.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,36 +7,24 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Retrieve MySQL credentials and database name from arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]  # The state name to search for
 
-    # Connect to the MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC"
+        .format(sys.argv[4])
+    )
 
-    # Create the SQL query using format to insert the state_name safely
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        state_name)
-
-    # Execute the SQL query
-    cursor.execute(query)
-
-    # Fetch and print each row in the result set
-    rows = cursor.fetchall()
+    rows = cur.fetchall()
     for row in rows:
         print(row)
 
-    # Close the cursor and the database connection
-    cursor.close()
+    cur.close()
     db.close()
