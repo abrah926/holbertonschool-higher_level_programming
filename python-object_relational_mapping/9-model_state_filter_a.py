445233a2a36486ebb3308105f3c7dda3e76d6dd9
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-11-03 11:19:10 -0400

    A script that lists all State objects that contain the letter 'a' from the database hbtn_0e_6_usa. It connects to a MySQL server running on localhost at port 3306 and displays the results in ascending order by states.id.

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+A script that lists all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa. It connects to a MySQL server
+running on localhost at port 3306 and displays the results in
+ascending order by states.id.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: ./9-model_state_filter_a.py <mysql_username> <mysql_password> <database_name>")
+        return
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    mysql_db_url = f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}"
+    engine = create_engine(mysql_db_url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states_with_a = session.query(State).filter(
+        State.name.contains('a')).order_by(State.id.asc()).all()
+
+    for state in states_with_a:
+        print(state.id, state.name)
+
+
+if __name__ == "__main__":
+    main()
