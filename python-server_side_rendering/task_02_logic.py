ce8629d7103eb3c3b080eeb198f0bb58f856c68d
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-11-09 21:13:38 -0400

    returning json file

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
 import json
-
-
+import os
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -22,7 +21,15 @@ def contact():
 
 @app.route('/items')
 def items():
-    return render_template('items.html')
+    json_file_path = os.path.join(os.path.dirname(__file__), 'items.json')
+    try:
+        with open(json_file_path, 'r') as file:
+            data = json.load(file)
+
+        items_list = data.get('items', [])
+    except (FileNotFoundError, json.JSONDecoderError):
+        items_list = []
+    return render_template('items.html', items=items_list)
 
 
 if __name__ == '__main__':
