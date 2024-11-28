9657ba740c46694c61a086265cff1a26ce7a777a
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-11-28 13:49:00 -0400

    defines state class

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Define a State class that links to the states table in the MySQL database."""
-
+"""Defines the State class and links to the MySQL table 'states'."""
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
-# Create a declarative base instance
+
 Base = declarative_base()
 
 
 class State(Base):
-    """State class that links to the states table in the database."""
+    """Class definition for State."""
+    __tablename__ = 'states'
 
-    __tablename__ = 'states'  # Name of the table in the database
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer,
+                primary_key=True,
+                autoincrement=True,
+                nullable=False,
+                unique=True
+                )
     name = Column(String(128), nullable=False)
-
-    def __repr__(self):
-        return f"<State(id={self.id}, name='{self.name}')>"
