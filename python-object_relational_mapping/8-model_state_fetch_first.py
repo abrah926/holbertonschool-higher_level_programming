259ae48e41641b61f15374ce2283604b93f9634d
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-11-28 13:52:49 -0400

    prints firts state obj

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,39 +1,26 @@
 #!/usr/bin/python3
 
-"""
-Script that prints the first State object from the database.
-"""
+""" script that prints the first State object
+from the database hbtn_0e_6_usa"""
 
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
-
-def main():
-    # Get the command-line arguments
-    if len(sys.argv) != 4:
-        print("Usage: ./8-model_state_fetch_first.py <mysql_username> <mysql_password> <database_name>")
-        return
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Create the SQLAlchemy engine and session
-    mysql_db_url = f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}"
-    engine = create_engine(mysql_db_url)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query for the first State object
-    first_state = session.query(State).order_by(State.id).first()
-
-    if first_state:
-        print(f"{first_state.id}: {first_state.name}")
-    else:
-        print("Nothing")
-
 
 if __name__ == "__main__":
-    main()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    session.close()
