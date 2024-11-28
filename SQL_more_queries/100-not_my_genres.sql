3e4f9ab04d49bd0aad77d57c6af0e34d04272c97
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-11-28 13:32:36 -0400

    corrected sql

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,15 @@
-SELECT tv_genres.name
+SELECT name
 FROM tv_genres
 WHERE
-    tv_genres.id NOT IN(
-        SELECT tv_show_genres.genre_id
+    id NOT IN(
+        SELECT genre_id
         FROM tv_show_genres
-            INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
         WHERE
-            tv_shows.title = 'Dexter'
+            show_id = (
+                SELECT id
+                FROM tv_shows
+                WHERE
+                    title = 'Dexter'
+            )
     )
-ORDER BY tv_genres.name ASC;
+ORDER BY name ASC;
